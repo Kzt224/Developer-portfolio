5199e3b48a19ef5463ae729cbcc9b62d0cddfe27
Kzt <tun81541@gmail.com>
2026-06-24 22:10:25 +0630

    modify_linkedin_link

--- src/data/site.js
+++ src/data/site.js
@@ -6,11 +6,10 @@ export const site = {
   tagline: "Building reliable systems and great product experiences.",
   email: "tun81541@gmail.com",
   bio: "Full-stack developer specializing in clean architecture, performant systems, and inclusive design. Experienced in delivering full-stack features and improving existing codebases. Seeking mid-level opportunities to take ownership of products and scale systems efficiently.",
-  //avatar: "https://api.dicebear.com/7.x/avataaars/svg?seed=alex",
   avatar: userImage,
   social: [
     { label: "GitHub", href: "https://github.com/Kzt224", icon: "github" },
-    { label: "LinkedIn", href: "https://www.linkedin.com/in/khin-zawtun-a1018b356?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app", icon: "linkedin" },
+    { label: "LinkedIn", href: "https://www.linkedin.com/in/khin-zaw-htun-a1018b356?utm_source=share_via&utm_content=profile&utm_medium=member_android", icon: "linkedin" },
     { label: "Facebook", href: "https://www.facebook.com/share/17j9zjNjm6/", icon: "facebook" },
   ],
 };
